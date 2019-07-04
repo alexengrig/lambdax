805e513fdd499a39f71079c3cc62708433c43846
Grig Alex <alexengrigdev@gmail.com>
2019-07-05 00:03:10 +0300

    Minor refactor ChainX test

--- src/test/java/alexengrig/lambdax/ChainXTest.java
+++ src/test/java/alexengrig/lambdax/ChainXTest.java
@@ -4,22 +4,24 @@ import alexengrig.lambdax.collection.MapX;
 import org.junit.Test;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 
 public class ChainXTest {
     @Test
     public void checkWorkWithMap() {
-        HashMap<Number, String> map = new HashMap<>();
-        int key = 3;
-        String value = "three";
+        Map<Number, CharSequence> map = new HashMap<>();
+        int one = 1;
+        int key = 2;
+        String value = "two";
         String actual = ChainX.of(map)
-                .peek(MapX.onlyPut(1, "one"))
-                .peek(MapX.onlyPut(2, "two"))
+                .peek(MapX.onlyPut(one, "one"))
+                .filter(MapX.containsByKey(one))
+                .peek(MapX.onlyRemove(one))
                 .peek(MapX.onlyPut(key, value))
-                .peek(MapX.onlyPut(4, "four"))
-                .peek(MapX.onlyRemove(1))
                 .map(MapX.get(key))
+                .map(String.class::cast)
                 .orElseThrow();
         assertEquals(value, actual);
     }
