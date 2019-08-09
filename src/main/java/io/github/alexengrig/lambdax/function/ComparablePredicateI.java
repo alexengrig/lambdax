932423ebb8b24988138d194ee138aa147f445171
Grig <alexengrigdev@gmail.com>
2019-08-09 22:30:20 +0300

    Refactor ComparablePredicateI
    Fixes #61

--- src/main/java/io/github/alexengrig/lambdax/function/ComparablePredicateI.java
+++ src/main/java/io/github/alexengrig/lambdax/function/ComparablePredicateI.java
@@ -1,32 +1,73 @@
 package io.github.alexengrig.lambdax.function;
 
-import java.util.function.Function;
+import java.util.Comparator;
 import java.util.function.Predicate;
 
 /**
- * <p>This interface describes a useful comparable predicate.</p>
+ * <p>This interface describes the useful predicate with the comparable result of the mapper.</p>
  *
- * @param <T> a type
- * @param <R> a result
+ * @param <T> the type of the input to the predicate
+ * @param <R> the type of the mapper comparable result
  * @author Grig Alex
  * @version 0.2.0
- * @see io.github.alexengrig.lambdax.function.PredicateI
  * @see java.lang.Comparable
+ * @see java.util.Comparator
  * @see java.util.function.Predicate
+ * @see io.github.alexengrig.lambdax.function.PredicateI
  * @since 0.2.0
  */
-/* package */interface ComparablePredicateI<T, R extends Comparable<R>> {
-    <V extends Comparable<V>> ComparablePredicateB<T, V> map(Function<R, V> mapper);
-
-    Predicate<T> less(R other);
-
-    Predicate<T> more(R other);
-
-    default Predicate<T> lessOrEquals(R other) {
-        return less(other).or(this::equals);
+/* package */interface ComparablePredicateI<T, R extends Comparable<R>> extends PredicateI<T, R> {
+    /**
+     * <p>
+     * Returns the {@link java.util.function.Predicate} that checks if the input object is less than the other object.
+     * </p>
+     *
+     * @param other an object for compare
+     * @return The {@link java.util.function.Predicate} with compare
+     * @since 0.2.0
+     */
+    default Predicate<T> less(R other) {
+        return less(other, Comparator.comparing(other::compareTo));
     }
 
-    default Predicate<T> moreOrEquals(R other) {
-        return more(other).or(this::equals);
+    /**
+     * <p>
+     * Returns the {@link java.util.function.Predicate} that checks if the input object is greater than the other object.
+     * </p>
+     *
+     * @param other an object for compare
+     * @return The {@link java.util.function.Predicate} with compare
+     * @since 0.2.0
+     */
+    default Predicate<T> greater(R other) {
+        return greater(other, Comparator.comparing(other::compareTo));
+    }
+
+    /**
+     * <p>
+     * Returns the {@link java.util.function.Predicate} that checks if the input object is less than or
+     * equal to the other object.
+     * </p>
+     *
+     * @param other an object for compare
+     * @return The {@link java.util.function.Predicate} with compare
+     * @since 0.2.0
+     */
+    default Predicate<T> lessOrEqual(R other) {
+        return lessOrEqual(other, Comparator.comparing(other::compareTo));
+    }
+
+    /**
+     * <p>
+     * Returns the {@link java.util.function.Predicate} that checks if the input object is greater than or
+     * equal to the other object.
+     * </p>
+     *
+     * @param other an object for compare
+     * @return The {@link java.util.function.Predicate} with compare
+     * @since 0.2.0
+     */
+    default Predicate<T> greaterOrEqual(R other) {
+        return greaterOrEqual(other, Comparator.comparing(other::compareTo));
     }
 }
