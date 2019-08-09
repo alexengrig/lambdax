0e9dfc11174720cce4d4f74dc7d946dfa771bd9b
Grig <alexengrigdev@gmail.com>
2019-08-09 22:17:06 +0300

    Refactor ComparablePredicateB
    Fixes #61

--- src/main/java/io/github/alexengrig/lambdax/function/ComparablePredicateB.java
+++ src/main/java/io/github/alexengrig/lambdax/function/ComparablePredicateB.java
@@ -1,42 +1,28 @@
 package io.github.alexengrig.lambdax.function;
 
 import java.util.function.Function;
-import java.util.function.Predicate;
 
 /**
- * <p>The base implementation of {@link io.github.alexengrig.lambdax.function.ComparablePredicateI} interface.</p>
+ * <p>The base implementation of the {@link io.github.alexengrig.lambdax.function.ComparablePredicateI} interface.</p>
  *
- * @param <T> a type
- * @param <R> a comparable result
+ * @param <T> the type of the input to the predicate
+ * @param <R> the type of the mapper comparable result
  * @author Grig Alex
  * @version 0.2.0
- * @see io.github.alexengrig.lambdax.function.ComparablePredicateI
  * @see java.lang.Comparable
- * @see java.util.function.Predicate
  * @see java.util.function.Function
+ * @see io.github.alexengrig.lambdax.function.ComparablePredicateI
+ * @see io.github.alexengrig.lambdax.function.PredicateB
  * @since 0.2.0
  */
-/* package */final class ComparablePredicateB<T, R extends Comparable<R>> implements ComparablePredicateI<T, R> {
-    private final static int ZERO = 0;
-
-    private final Function<T, R> function;
-
+/* package */final class ComparablePredicateB<T, R extends Comparable<R>> extends PredicateB<T, R> implements ComparablePredicateI<T, R> {
+    /**
+     * <p>The constructor with the mapper.</p>
+     *
+     * @param mapper a function for map the predicate input to the comparable result
+     * @since 0.2.0
+     */
     /* package */ComparablePredicateB(Function<T, R> mapper) {
-        function = mapper;
-    }
-
-    @Override
-    public <V extends Comparable<V>> ComparablePredicateB<T, V> map(Function<R, V> mapper) {
-        return new ComparablePredicateB<>(function.andThen(mapper));
-    }
-
-    @Override
-    public Predicate<T> less(R other) {
-        return t -> function.apply(t).compareTo(other) < ZERO;
-    }
-
-    @Override
-    public Predicate<T> more(R other) {
-        return t -> function.apply(t).compareTo(other) > ZERO;
+        super(mapper);
     }
 }
