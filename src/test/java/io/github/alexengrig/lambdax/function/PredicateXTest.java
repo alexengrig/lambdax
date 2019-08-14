b371319a230cad817c1d2a29bef00143f359d466
Grig Alex <alexengrigdev@gmail.com>
2019-08-14 20:46:04 +0300

    Add chaining tests
    Fixes #61

--- src/test/java/io/github/alexengrig/lambdax/function/PredicateXTest.java
+++ src/test/java/io/github/alexengrig/lambdax/function/PredicateXTest.java
@@ -23,7 +23,10 @@ import io.github.alexengrig.lambdax.example.Pack;
 import org.junit.Test;
 
 import java.util.Comparator;
+import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -42,6 +45,20 @@ public class PredicateXTest {
     }
 
     @Test
+    public void checkCheckerChaining() {
+        String value = "Coca-Cola";
+        Holder<Box> boxHolder = new Holder<>(new Box(new Pack(new Item(value))));
+        List<Holder<Box>> list = Stream.of(boxHolder)
+                .filter(PredicateX.of(Holder<Box>::get)
+                        .map(Box::getPack)
+                        .map(Pack::getItem)
+                        .map(Item::getName)
+                        .check(String::isEmpty))
+                .collect(Collectors.toList());
+        assertTrue(list.isEmpty());
+    }
+
+    @Test
     public void checkEqual() {
         String value = "Coca-Cola";
         Pack pack = new Pack(new Item(value));
@@ -52,6 +69,18 @@ public class PredicateXTest {
     }
 
     @Test
+    public void checkEqualChaining() {
+        String value = "Coca-Cola";
+        Pack pack = new Pack(new Item(value));
+        List<Pack> list = Stream.of(pack)
+                .filter(PredicateX.of(Pack::getItem)
+                        .map(Item::getName)
+                        .equal(value))
+                .collect(Collectors.toList());
+        assertFalse(list.isEmpty());
+    }
+
+    @Test
     public void checkLessWithComparator() {
         String value = "Coca-Cola";
         Box box = new Box(new Pack(new Item(value)));
@@ -59,6 +88,18 @@ public class PredicateXTest {
         Predicate<Box> lessPepsi = PredicateX.of(Box::getPack)
                 .less(pack, Comparator.comparing(p -> p.getItem().getName()));
         assertTrue(lessPepsi.test(box));
+    }
+
+    @Test
+    public void checkLessWithComparatorChaining() {
+        String value = "Coca-Cola";
+        Box box = new Box(new Pack(new Item(value)));
+        Pack pack = new Pack(new Item("Pepsi"));
+        List<Box> list = Stream.of(box)
+                .filter(PredicateX.of(Box::getPack)
+                        .less(pack, Comparator.comparing(p -> p.getItem().getName())))
+                .collect(Collectors.toList());
+        assertFalse(list.isEmpty());
     }
 
     @Test
@@ -73,6 +114,19 @@ public class PredicateXTest {
     }
 
     @Test
+    public void checkLessChaining() {
+        String value = "Coca-Cola";
+        Box box = new Box(new Pack(new Item(value)));
+        List<Box> list = Stream.of(box)
+                .filter(PredicateX.of(Box::getPack)
+                        .map(Pack::getItem)
+                        .map(Item::getName)
+                        .less("Pepsi"))
+                .collect(Collectors.toList());
+        assertFalse(list.isEmpty());
+    }
+
+    @Test
     public void checkGreaterWithComparator() {
         String value = "Schweppes";
         Box box = new Box(new Pack(new Item(value)));
@@ -80,6 +134,18 @@ public class PredicateXTest {
         Predicate<Box> greaterCocaCola = PredicateX.of(Box::getPack)
                 .greater(pack, Comparator.comparing(p -> p.getItem().getName()));
         assertTrue(greaterCocaCola.test(box));
+    }
+
+    @Test
+    public void checkGreaterWithComparatorChaining() {
+        String value = "Schweppes";
+        Box box = new Box(new Pack(new Item(value)));
+        Pack pack = new Pack(new Item("Dr Pepper"));
+        List<Box> list = Stream.of(box)
+                .filter(PredicateX.of(Box::getPack)
+                        .greater(pack, Comparator.comparing(p -> p.getItem().getName())))
+                .collect(Collectors.toList());
+        assertFalse(list.isEmpty());
     }
 
     @Test
@@ -94,6 +160,19 @@ public class PredicateXTest {
     }
 
     @Test
+    public void checkGreaterChaining() {
+        String value = "Schweppes";
+        Box box = new Box(new Pack(new Item(value)));
+        List<Box> list = Stream.of(box)
+                .filter(PredicateX.of(Box::getPack)
+                        .map(Pack::getItem)
+                        .map(Item::getName)
+                        .greater("Dr Pepper"))
+                .collect(Collectors.toList());
+        assertFalse(list.isEmpty());
+    }
+
+    @Test
     public void checkLessOrEqual() {
         String value = "Fanta";
         Box box = new Box(new Pack(new Item(value)));
@@ -105,6 +184,19 @@ public class PredicateXTest {
     }
 
     @Test
+    public void checkLessOrEqualChaining() {
+        String value = "Fanta";
+        Box box = new Box(new Pack(new Item(value)));
+        List<Box> list = Stream.of(box)
+                .filter(PredicateX.of(Box::getPack)
+                        .map(Pack::getItem)
+                        .map(Item::getName)
+                        .lessOrEqual("Mirinda"))
+                .collect(Collectors.toList());
+        assertFalse(list.isEmpty());
+    }
+
+    @Test
     public void checkGreaterOrEqual() {
         String value = "Sprite";
         Box box = new Box(new Pack(new Item(value)));
@@ -113,5 +205,18 @@ public class PredicateXTest {
                 .map(Item::getName)
                 .greaterOrEqual("7 Up");
         assertTrue(greaterCocaCola.test(box));
+    }
+
+    @Test
+    public void checkGreaterOrEqualChaining() {
+        String value = "Sprite";
+        Box box = new Box(new Pack(new Item(value)));
+        List<Box> list = Stream.of(box)
+                .filter(PredicateX.of(Box::getPack)
+                        .map(Pack::getItem)
+                        .map(Item::getName)
+                        .greaterOrEqual("7 Up"))
+                .collect(Collectors.toList());
+        assertFalse(list.isEmpty());
     }
 }
