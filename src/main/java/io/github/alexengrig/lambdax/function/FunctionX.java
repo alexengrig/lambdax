4f73abce6b71a7ab2f62f79c9506c2ede9f45a63
Grig Alex <alexengrigdev@gmail.com>
2019-08-26 21:18:58 +0300

    Rename FunctionX#ofNullable to FunctionX#nullSafe

--- src/main/java/io/github/alexengrig/lambdax/function/FunctionX.java
+++ src/main/java/io/github/alexengrig/lambdax/function/FunctionX.java
@@ -18,11 +18,36 @@ package io.github.alexengrig.lambdax.function;
 
 import java.util.function.Function;
 
+/**
+ * <p>This utility class contains useful lambdas for {@link java.util.function.Function}.</p>
+ *
+ * @author Grig Alex
+ * @version 0.2.1
+ * @see java.util.function.Function
+ * @since 0.2.1
+ */
 public class FunctionX {
+    /**
+     * <p>The private constructor.</p>
+     *
+     * @since 0.2.1
+     */
     private FunctionX() {
     }
 
-    public static <T, R> Function<T, R> ofNullable(Function<? super T, ? extends R> function) {
+    /**
+     * <p>Returns the {@link java.util.function.Function} that is if the input value is null
+     * then returns null, otherwise returns the result of the input function.</p>
+     *
+     * @param <T>      the type of the input to the function
+     * @param <R>      the type of the result of the function
+     * @param function the function to apply the input value
+     * @return The {@link java.util.function.Function} with the null safety input value
+     * @see java.util.function.Function
+     * @see java.util.function.Function#apply(Object)
+     * @since 0.2.1
+     */
+    public static <T, R> Function<T, R> nullSafe(Function<? super T, ? extends R> function) {
         return t -> t == null ? null : function.apply(t);
     }
 }
