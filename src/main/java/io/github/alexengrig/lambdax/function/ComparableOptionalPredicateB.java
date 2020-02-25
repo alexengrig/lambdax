556769128d774043a71f8e3ba3a947b7cd6da181
Grig Alex <alexengrigdev@gmail.com>
2020-02-25 21:35:07 +0300

    Add implementation to ComparableOptionalPredicateB

--- src/main/java/io/github/alexengrig/lambdax/function/ComparableOptionalPredicateB.java
+++ src/main/java/io/github/alexengrig/lambdax/function/ComparableOptionalPredicateB.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 LambdaX contributors
+ * Copyright 2019 - 2020 Alexengrig Dev.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,105 +16,46 @@
 
 package io.github.alexengrig.lambdax.function;
 
-import java.util.Comparator;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
 /**
  * TODO: Add JavaDoc
  */
-/* package */ class ComparableOptionalPredicateB<T, R extends Comparable<R>>
-    implements ComparableOptionalPredicateI<T, R> {
-  /**
-   * TODO: Add JavaDoc
-   */
-  protected final Function<T, R> function;
+/* package */class ComparableOptionalPredicateB<T, R extends Comparable<R>> implements ComparableOptionalPredicateI<T, R> {
+    /**
+     * TODO: Add JavaDoc
+     */
+    protected final Function<T, R> function;
 
-  /**
-   * TODO: Add JavaDoc
-   */
-  /* package */ ComparableOptionalPredicateB(Function<T, R> mapper) {
-    function = mapper;
-  }
+    /**
+     * TODO: Add JavaDoc
+     */
+    /* package */ComparableOptionalPredicateB(Function<T, R> mapper) {
+        function = mapper;
+    }
 
-  /**
-   * TODO: Add JavaDoc
-   */
-  @Override
-  public <V> OptionalPredicateI<T, V> map(Function<R, V> mapper) {
-    return new OptionalPredicateB<>(function.andThen(mapper));
-  }
+    /**
+     * TODO: Add JavaDoc
+     */
+    @Override
+    public <V> OptionalPredicateI<T, V> map(Function<R, V> mapper) {
+        return new OptionalPredicateB<>(function.andThen(FunctionX.nullSafe(mapper)));
+    }
 
-  /**
-   * TODO: Add JavaDoc
-   */
-  @Override
-  public <V extends Comparable<V>> ComparableOptionalPredicateI<T, V>
-  map(ComparableResultFunction<R, V> mapper) {
-    return new ComparableOptionalPredicateB<>(function.andThen(mapper));
-  }
+    /**
+     * TODO: Add JavaDoc
+     */
+    @Override
+    public <V extends Comparable<V>> ComparableOptionalPredicateI<T, V> map(ComparableResultFunction<R, V> mapper) {
+        return new ComparableOptionalPredicateB<>(function.andThen(FunctionX.nullSafe(mapper)));
+    }
 
-  /**
-   * TODO: Add JavaDoc
-   */
-  @Override
-  public Predicate<T> isNull() {
-    return null; // TODO: Add implementation
-  }
-
-  /**
-   * TODO: Add JavaDoc
-   */
-  @Override
-  public Predicate<T> nonNull() {
-    return null; // TODO: Add implementation
-  }
-
-  /**
-   * TODO: Add JavaDoc
-   */
-  @Override
-  public ResultI<T> check(Predicate<R> checker) {
-    return null; // TODO: Add implementation
-  }
-
-  /**
-   * TODO: Add JavaDoc
-   */
-  @Override
-  public ResultI<T> equal(R other) {
-    return null; // TODO: Add implementation
-  }
-
-  /**
-   * TODO: Add JavaDoc
-   */
-  @Override
-  public ResultI<T> less(R other, Comparator<R> comparator) {
-    return null; // TODO: Add implementation
-  }
-
-  /**
-   * TODO: Add JavaDoc
-   */
-  @Override
-  public ResultI<T> greater(R other, Comparator<R> comparator) {
-    return null; // TODO: Add implementation
-  }
-
-  /**
-   * TODO: Add JavaDoc
-   */
-  @Override
-  public ResultI<T> lessOrEqual(R other, Comparator<R> comparator) {
-    return null; // TODO: Add implementation
-  }
-
-  /**
-   * TODO: Add JavaDoc
-   */
-  @Override
-  public ResultI<T> greaterOrEqual(R other, Comparator<R> comparator) {
-    return null; // TODO: Add implementation
-  }
+    /**
+     * TODO: Add JavaDoc
+     */
+    @Override
+    public OptionalPredicateResultI<T> check(Predicate<R> checker) {
+        return new OptionalPredicateResultB<>(function, checker);
+    }
 }
