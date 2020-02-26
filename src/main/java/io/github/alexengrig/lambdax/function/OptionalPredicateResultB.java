1c677e450056397baf4dd171c4840407a7a5cc02
Grig Alex <alexengrigdev@gmail.com>
2020-02-26 20:38:02 +0300

    Add default methods implementations to OptionalPredicateResultB

--- src/main/java/io/github/alexengrig/lambdax/function/OptionalPredicateResultB.java
+++ src/main/java/io/github/alexengrig/lambdax/function/OptionalPredicateResultB.java
@@ -19,15 +19,31 @@ package io.github.alexengrig.lambdax.function;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
+/**
+ * TODO: Add JavaDoc
+ */
 /* package */class OptionalPredicateResultB<T, V> implements OptionalPredicateResultI<T> {
-    private final Function<T, V> function;
-    private final Predicate<V> predicate;
+    /**
+     * TODO: Add JavaDoc
+     */
+    protected final Function<T, V> function;
 
+    /**
+     * TODO: Add JavaDoc
+     */
+    protected final Predicate<V> predicate;
+
+    /**
+     * TODO: Add JavaDoc
+     */
     /* package */OptionalPredicateResultB(Function<T, V> function, Predicate<V> predicate) {
         this.function = function;
         this.predicate = predicate;
     }
 
+    /**
+     * TODO: Add JavaDoc
+     */
     @Override
     public Predicate<T> orElse(Predicate<T> checker) {
         return t -> {
@@ -36,6 +52,51 @@ import java.util.function.Predicate;
                 return predicate.test(value);
             } else {
                 return checker.test(t);
+            }
+        };
+    }
+
+    /**
+     * TODO: Add JavaDoc
+     */
+    @Override
+    public Predicate<T> orElse(boolean check) {
+        return t -> {
+            V value = function.apply(t);
+            if (value != null) {
+                return predicate.test(value);
+            } else {
+                return check;
+            }
+        };
+    }
+
+    /**
+     * TODO: Add JavaDoc
+     */
+    @Override
+    public Predicate<T> orTruth() {
+        return t -> {
+            V value = function.apply(t);
+            if (value != null) {
+                return predicate.test(value);
+            } else {
+                return true;
+            }
+        };
+    }
+
+    /**
+     * TODO: Add JavaDoc
+     */
+    @Override
+    public Predicate<T> orLie() {
+        return t -> {
+            V value = function.apply(t);
+            if (value != null) {
+                return predicate.test(value);
+            } else {
+                return false;
             }
         };
     }
