f32cf1dab1f2132456729d2e1c70d273f321065e
Grig Alex <alexengrigdev@gmail.com>
2020-03-19 22:25:18 +0300

    Add logic operations
    and, or, xor, nand, nor, xnor

--- src/main/java/io/github/alexengrig/lambdax/function/PredicateX.java
+++ src/main/java/io/github/alexengrig/lambdax/function/PredicateX.java
@@ -23,7 +23,7 @@ import java.util.function.Predicate;
  * <p>This utility class contains useful lambdas for {@link java.util.function.Predicate}.</p>
  *
  * @author Grig Alex
- * @version 0.2.1
+ * @version 0.3.0
  * @see java.util.function.Function
  * @see java.util.function.Predicate
  * @see io.github.alexengrig.lambdax.function.ComparableResultFunction
@@ -78,9 +78,267 @@ public final class PredicateX {
      * @see java.util.function.Predicate#negate
      * @since 0.2.0
      */
-    @SuppressWarnings("unchecked")
-    public static <T> Predicate<T> not(Predicate<? super T> predicate) {
-        return (Predicate<T>) predicate.negate();
+    public static <T> Predicate<T> not(Predicate<T> predicate) {
+        return predicate.negate();
+    }
+
+    /**
+     * Returns a composed predicate that represents a short-circuiting logical
+     * AND of first predicate and second predicate.
+     * When evaluating the composed predicate,
+     * if first predicate is {@code false},
+     * then second predicate is not evaluated.
+     *
+     * @param <T>    the type of arguments to the predicate
+     * @param first  a predicate that will be logically-ANDed with the {@code second}
+     * @param second a predicate that will be logically-ANDed with the {@code first}
+     * @return a composed predicate that represents the short-circuiting logical
+     * AND of the {@code first} predicate and the {@code second} predicate
+     * @see java.util.function.Predicate
+     * @see java.util.function.Predicate#and(java.util.function.Predicate)
+     * @since 0.3.0
+     */
+    public static <T> Predicate<T> and(Predicate<T> first, Predicate<T> second) {
+        return t -> first.test(t) && second.test(t);
+    }
+
+    /**
+     * Returns a composed predicate that represents a short-circuiting logical AND of predicates.
+     * When evaluating the composed predicate,
+     * if any predicate is {@code false},
+     * then those following predicates are not evaluated.
+     *
+     * @param <T>    the type of arguments to the predicate
+     * @param first  a predicate that will be logically-ANDed with the {@code second}
+     * @param second a predicate that will be logically-ANDed with the {@code first}
+     * @param others predicates that will be logically-ANDed with previous result
+     *               starting from the result of the {@code first} and the {@code first}
+     * @return a composed predicate that represents the short-circuiting logical
+     * AND of the {@code first} predicate, the {@code second} predicate and the {@code others} predicates
+     * @see java.util.function.Predicate
+     * @see java.util.function.Predicate#and(java.util.function.Predicate)
+     * @see io.github.alexengrig.lambdax.function.PredicateX#and(java.util.function.Predicate, java.util.function.Predicate)
+     * @since 0.3.0
+     */
+    @SafeVarargs
+    public static <T> Predicate<T> and(Predicate<T> first, Predicate<T> second, Predicate<T>... others) {
+        Predicate<T> predicate = and(first, second);
+        for (Predicate<T> other : others) {
+            predicate = and(predicate, other);
+        }
+        return predicate;
+    }
+
+    /**
+     * Returns a composed predicate that represents a short-circuiting logical
+     * OR of first predicate and second predicate.
+     * When evaluating the composed predicate,
+     * if first predicate is {@code true},
+     * then second predicate is not evaluated.
+     *
+     * @param <T>    the type of arguments to the predicate
+     * @param first  a predicate that will be logically-ORed with the {@code second}
+     * @param second a predicate that will be logically-ORed with the {@code first}
+     * @return a composed predicate that represents the short-circuiting logical
+     * OR of the {@code first} predicate and the {@code second} predicate
+     * @see java.util.function.Predicate
+     * @see java.util.function.Predicate#or(java.util.function.Predicate)
+     * @since 0.3.0
+     */
+    public static <T> Predicate<T> or(Predicate<T> first, Predicate<T> second) {
+        return t -> first.test(t) || second.test(t);
+    }
+
+    /**
+     * Returns a composed predicate that represents a short-circuiting logical OR of predicates.
+     * When evaluating the composed predicate,
+     * if any predicate is {@code true},
+     * then those following predicates are not evaluated.
+     *
+     * @param <T>    the type of arguments to the predicate
+     * @param first  a predicate that will be logically-ORed with the {@code second}
+     * @param second a predicate that will be logically-ORed with the {@code first}
+     * @param others predicates that will be logically-ORed with previous result
+     *               starting from the result of the {@code first} and the {@code first}
+     * @return a composed predicate that represents the short-circuiting logical
+     * OR of the {@code first} predicate, the {@code second} predicate and the {@code others} predicates
+     * @see java.util.function.Predicate
+     * @see java.util.function.Predicate#or(java.util.function.Predicate)
+     * @see io.github.alexengrig.lambdax.function.PredicateX#or(java.util.function.Predicate, java.util.function.Predicate)
+     * @since 0.3.0
+     */
+    @SafeVarargs
+    public static <T> Predicate<T> or(Predicate<T> first, Predicate<T> second, Predicate<T>... others) {
+        Predicate<T> predicate = first.or(second);
+        for (Predicate<T> other : others) {
+            predicate = predicate.or(other);
+        }
+        return predicate;
+    }
+
+    /**
+     * Returns a composed predicate that represents a short-circuiting logical
+     * XOR of first predicate and second predicate.
+     *
+     * @param <T>    the type of arguments to the predicate
+     * @param first  a predicate that will be logically-XORed with the {@code second}
+     * @param second a predicate that will be logically-XORed with the {@code first}
+     * @return a composed predicate that represents the short-circuiting logical
+     * XOR of the {@code first} predicate and the {@code second} predicate
+     * @see java.util.function.Predicate
+     * @since 0.3.0
+     */
+    public static <T> Predicate<T> xor(Predicate<T> first, Predicate<T> second) {
+        return t -> first.test(t) ^ second.test(t);
+    }
+
+    /**
+     * Returns a composed predicate that represents a short-circuiting logical XOR of predicates.
+     *
+     * @param <T>    the type of arguments to the predicate
+     * @param first  a predicate that will be logically-XORed with the {@code second}
+     * @param second a predicate that will be logically-XORed with the {@code first}
+     * @param others predicates that will be logically-XORed with previous result
+     *               starting from the result of the {@code first} and the {@code first}
+     * @return a composed predicate that represents the short-circuiting logical
+     * XOR of the {@code first} predicate, the {@code second} predicate and the {@code others} predicates
+     * @see java.util.function.Predicate
+     * @see io.github.alexengrig.lambdax.function.PredicateX#xor(java.util.function.Predicate, java.util.function.Predicate)
+     * @since 0.3.0
+     */
+    @SafeVarargs
+    public static <T> Predicate<T> xor(Predicate<T> first, Predicate<T> second, Predicate<T>... others) {
+        Predicate<T> predicate = xor(first, second);
+        for (Predicate<T> other : others) {
+            predicate = xor(predicate, other);
+        }
+        return predicate;
+    }
+
+    /**
+     * Returns a composed predicate that represents a short-circuiting logical
+     * NAND of first predicate and second predicate.
+     * When evaluating the composed predicate,
+     * if first predicate is {@code false},
+     * then second predicate is not evaluated.
+     *
+     * @param <T>    the type of arguments to the predicate
+     * @param first  a predicate that will be logically-NANDed with the {@code second}
+     * @param second a predicate that will be logically-NANDed with the {@code first}
+     * @return a composed predicate that represents the short-circuiting logical
+     * NAND of the {@code first} predicate and the {@code second} predicate
+     * @see java.util.function.Predicate
+     * @since 0.3.0
+     */
+    public static <T> Predicate<T> nand(Predicate<T> first, Predicate<T> second) {
+        return t -> !(first.test(t) && second.test(t));
+    }
+
+    /**
+     * Returns a composed predicate that represents a short-circuiting logical NAND of predicates.
+     * if any predicate is {@code false},
+     * then those following predicates are not evaluated.
+     *
+     * @param <T>    the type of arguments to the predicate
+     * @param first  a predicate that will be logically-NANDed with the {@code second}
+     * @param second a predicate that will be logically-NANDed with the {@code first}
+     * @param others predicates that will be logically-NANDed with previous result
+     *               starting from the result of the {@code first} and the {@code first}
+     * @return a composed predicate that represents the short-circuiting logical
+     * NAND of the {@code first} predicate, the {@code second} predicate and the {@code others} predicates
+     * @see java.util.function.Predicate
+     * @see io.github.alexengrig.lambdax.function.PredicateX#nand(java.util.function.Predicate, java.util.function.Predicate)
+     * @since 0.3.0
+     */
+    @SafeVarargs
+    public static <T> Predicate<T> nand(Predicate<T> first, Predicate<T> second, Predicate<T>... others) {
+        Predicate<T> predicate = nand(first, second);
+        for (Predicate<T> other : others) {
+            predicate = nand(predicate, other);
+        }
+        return predicate;
+    }
+
+    /**
+     * Returns a composed predicate that represents a short-circuiting logical
+     * NOR of first predicate and second predicate.
+     * When evaluating the composed predicate,
+     * if first predicate is {@code true},
+     * then second predicate is not evaluated.
+     *
+     * @param <T>    the type of arguments to the predicate
+     * @param first  a predicate that will be logically-NORed with the {@code second}
+     * @param second a predicate that will be logically-NORed with the {@code first}
+     * @return a composed predicate that represents the short-circuiting logical
+     * NOR of the {@code first} predicate and the {@code second} predicate
+     * @see java.util.function.Predicate
+     * @since 0.3.0
+     */
+    public static <T> Predicate<T> nor(Predicate<T> first, Predicate<T> second) {
+        return t -> !(first.test(t) || second.test(t));
+    }
+
+    /**
+     * Returns a composed predicate that represents a short-circuiting logical NOR of predicates.
+     * When evaluating the composed predicate,
+     * if any predicate is {@code true},
+     * then those following predicates are not evaluated.
+     *
+     * @param <T>    the type of arguments to the predicate
+     * @param first  a predicate that will be logically-NORed with the {@code second}
+     * @param second a predicate that will be logically-NORed with the {@code first}
+     * @param others predicates that will be logically-NORed with previous result
+     *               starting from the result of the {@code first} and the {@code first}
+     * @return a composed predicate that represents the short-circuiting logical
+     * NOR of the {@code first} predicate, the {@code second} predicate and the {@code others} predicates
+     * @see java.util.function.Predicate
+     * @see io.github.alexengrig.lambdax.function.PredicateX#nor(java.util.function.Predicate, java.util.function.Predicate)
+     * @since 0.3.0
+     */
+    @SafeVarargs
+    public static <T> Predicate<T> nor(Predicate<T> first, Predicate<T> second, Predicate<T>... others) {
+        Predicate<T> predicate = nor(first, second);
+        for (Predicate<T> other : others) {
+            predicate = nor(predicate, other);
+        }
+        return predicate;
+    }
+
+    /**
+     * Returns a composed predicate that represents a short-circuiting logical
+     * XNOR of first predicate and second predicate.
+     *
+     * @param <T>    the type of arguments to the predicate
+     * @param first  a predicate that will be logically-XNORed with the {@code second}
+     * @param second a predicate that will be logically-XNORed with the {@code first}
+     * @return a composed predicate that represents the short-circuiting logical
+     * XNOR of the {@code first} predicate and the {@code second} predicate
+     * @see java.util.function.Predicate
+     * @since 0.3.0
+     */
+    public static <T> Predicate<T> xnor(Predicate<T> first, Predicate<T> second) {
+        return t -> first.test(t) == second.test(t);
+    }
+
+    /**
+     * Returns a composed predicate that represents a short-circuiting logical
+     * XNOR of first predicate and second predicate.
+     *
+     * @param <T>    the type of arguments to the predicate
+     * @param first  a predicate that will be logically-XNORed with the {@code second}
+     * @param second a predicate that will be logically-XNORed with the {@code first}
+     * @return a composed predicate that represents the short-circuiting logical
+     * XNOR of the {@code first} predicate and the {@code second} predicate
+     * @see java.util.function.Predicate
+     * @since 0.3.0
+     */
+    @SafeVarargs
+    public static <T> Predicate<T> xnor(Predicate<T> first, Predicate<T> second, Predicate<T>... others) {
+        Predicate<T> predicate = xnor(first, second);
+        for (Predicate<T> other : others) {
+            predicate = xnor(predicate, other);
+        }
+        return predicate;
     }
 
     /**
