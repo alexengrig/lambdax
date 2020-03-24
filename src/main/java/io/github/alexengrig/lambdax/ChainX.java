c0c1ff64bf6ace7a821f108248e8ad9009165152
Grig Alex <alexengrigdev@gmail.com>
2020-03-24 22:18:02 +0300

    Add JavaDoc

--- src/main/java/io/github/alexengrig/lambdax/ChainX.java
+++ src/main/java/io/github/alexengrig/lambdax/ChainX.java
@@ -109,28 +109,18 @@ public class ChainX<T> {
         return new ChainX<>(producer.get());
     }
 
-
     /**
-     * Returns an {@code ChainX} storing the nullable obtained value from the {@code Optional}.
+     * Returns an {@code ChainX} storing the nullable obtained value from the {@link java.util.Optional}.
      *
-     * @param optional the {@code Optional} of the nullable value to store
-     * @param <T>      the type of the nullable obtained value from the {@code optional}
-     * @return A {@code ChainX} with the nullable obtained value from {@code optional}
+     * @param optional the {@link java.util.Optional} of the nullable value to store
+     * @param <T>      the type of the nullable obtained value from the {@link java.util.Optional}
+     * @return A {@code ChainX} with the nullable obtained value from {@link java.util.Optional}
+     * @see java.util.Optional
      * @since 0.4.0
      */
     @SuppressWarnings({"unchecked", "OptionalUsedAsFieldOrParameterType"})
     public static <T> ChainX<T> of(Optional<? extends T> optional) {
         return (ChainX<T>) optional.map(ChainX::new).orElseGet(ChainX::empty);
-    }
-
-    /**
-     * Returns the stored value.
-     *
-     * @return The stored value
-     * @since 0.4.0
-     */
-    public T get() {
-        return value;
     }
 
     /**
@@ -153,6 +143,19 @@ public class ChainX<T> {
         return value != null;
     }
 
+    /**
+     * If a value is not {@code null},
+     * and the value matches the given predicate,
+     * returns a {@code ChainX} storing the value,
+     * otherwise returns an empty {@code ChainX}.
+     *
+     * @param predicate the predicate to apply to a value, if not {@code null}
+     * @return a {@code ChainX} storing the value of this
+     * {@code ChainX}, if a value is not {@code null} and the value matches the
+     * given predicate, otherwise an empty {@code ChainX}
+     * @see java.util.function.Predicate
+     * @since 0.4.0
+     */
     public ChainX<T> filter(Predicate<? super T> predicate) {
         if (isNull() || predicate.test(value)) {
             return this;
@@ -160,6 +163,17 @@ public class ChainX<T> {
         return empty();
     }
 
+    /**
+     * Returns this {@code ChainX},
+     * if a value is not {@code null}, the given consumer function updates the value.
+     *
+     * <p>ATTENTION: This method mutates the value.
+     *
+     * @param mutator the consumer function to accept to a value, if not {@code null}
+     * @return this {@code ChainX} storing the updates value
+     * @see java.util.function.Consumer
+     * @since 0.4.0
+     */
     public ChainX<T> mutate(Consumer<? super T> mutator) {
         if (nonNull()) {
             mutator.accept(value);
@@ -167,6 +181,17 @@ public class ChainX<T> {
         return this;
     }
 
+    /**
+     * If a value is not {@code null},
+     * returns a {@code ChainX} storing (as if by {@link #of}) the result of applying
+     * the given mapping function to the value, otherwise returns an empty {@code ChainX}.
+     *
+     * @param mapper the mapping function to apply to a value, if not {@code null}
+     * @param <R>    the type of the value returned from the mapping function
+     * @return a {@code ChainX} storing the result of applying a mapping
+     * function to the value of this {@code ChainX}
+     * @since 0.4.0
+     */
     public <R> ChainX<R> map(Function<? super T, ? extends R> mapper) {
         if (nonNull()) {
             return of(mapper.apply(value));
@@ -174,6 +199,22 @@ public class ChainX<T> {
         return empty();
     }
 
+    /**
+     * If a value is not {@code null},
+     * returns the result of applying the given {@code ChainX}-bearing mapping function to the value,
+     * otherwise returns an empty {@code ChainX}.
+     *
+     * <p>This method is similar to {@link #map(Function)},
+     * but the mapping function is one whose result is already a {@code ChainX},
+     * and if invoked, {@code flatMap} does not wrap it within an additional {@code ChainX}.
+     *
+     * @param mapper the mapping function to apply to a value, if not {@code null}
+     * @param <R>    the type of value of the {@code ChainX} returned by the mapping function
+     * @return the result of applying a {@code ChainX}-bearing mapping
+     * function to the value of this {@code ChainX}, if a value is not {@code null}, otherwise an empty {@code ChainX}
+     * @see java.util.function.Function
+     * @since 0.4.0
+     */
     @SuppressWarnings("unchecked")
     public <R> ChainX<R> flatMap(Function<? super T, ? extends ChainX<? extends R>> mapper) {
         if (nonNull()) {
@@ -182,6 +223,19 @@ public class ChainX<T> {
         return empty();
     }
 
+    /**
+     * If a value is not {@code null},
+     * returns the result of applying the given {@link java.util.Optional}-bearing mapping function to the value,
+     * otherwise returns an empty {@code ChainX}.
+     *
+     * @param mapper the mapping function to apply to a value, if not {@code null}
+     * @param <R>    the type of value of the {@link java.util.Optional} returned by the mapping function
+     * @return the result of applying a {@link java.util.Optional}-bearing mapping
+     * function to the value of this {@code ChainX}, if a value is not {@code null}, otherwise an empty {@code ChainX}
+     * @see io.github.alexengrig.lambdax.function.OptionalPredicateResultI
+     * @see java.util.Optional
+     * @since 0.4.0
+     */
     @SuppressWarnings("unchecked")
     public <R> ChainX<R> flatMap(OptionalResultFunction<? super T, ? extends R> mapper) {
         if (nonNull()) {
@@ -191,6 +245,16 @@ public class ChainX<T> {
         return empty();
     }
 
+    /**
+     * If a value is not {@code null},
+     * returns this {@code ChainX} storing the value,
+     * otherwise returns the given {@code ChainX}.
+     *
+     * @param chain the {@code ChainX} to be returned
+     * @return returns this {@code ChainX} storing the value of this {@code ChainX},
+     * if a value is not {@code null}, otherwise the given {@code ChainX}
+     * @since 0.4.0
+     */
     @SuppressWarnings("unchecked")
     public ChainX<T> or(ChainX<? extends T> chain) {
         if (isNull()) {
@@ -199,6 +263,17 @@ public class ChainX<T> {
         return this;
     }
 
+    /**
+     * If a value is not {@code null},
+     * returns this {@code ChainX} storing the value,
+     * otherwise returns a {@code ChainX} produced by the producer function.
+     *
+     * @param producer the supplying function that produces a {@code ChainX} to be returned
+     * @return returns this {@code ChainX} storing the value of this {@code ChainX},
+     * if a value is not {@code null}, otherwise a {@code ChainX} produced by the supplying function
+     * @see java.util.function.Supplier
+     * @since 0.4.0
+     */
     @SuppressWarnings("unchecked")
     public ChainX<T> or(Supplier<? extends ChainX<? extends T>> producer) {
         if (isNull()) {
@@ -207,12 +282,42 @@ public class ChainX<T> {
         return this;
     }
 
+    /**
+     * If a value is not {@code null},
+     * returns a sequential {@link Stream} containing only that value,
+     * otherwise returns an empty {@code Stream}.
+     *
+     * @return the value as a {@code Stream}
+     * @see java.util.stream.Stream
+     * @since 0.4.0
+     */
     public Stream<T> stream() {
         return nonNull() ? Stream.of(value) : Stream.empty();
     }
 
+    /**
+     * If a value is not {@code null},
+     * returns an {@link java.util.Optional} containing that value,
+     * otherwise returns an empty {@link java.util.Optional}.
+     *
+     * @return the value as an {@link java.util.Optional}
+     * @see java.util.Optional
+     * @since 0.4.0
+     */
     public Optional<T> optional() {
         return nonNull() ? Optional.of(value) : Optional.empty();
+    }
+
+    /**
+     * Returns the stored value.
+     *
+     * <p>ATTENTION: The value might be null.
+     *
+     * @return The stored value
+     * @since 0.4.0
+     */
+    public T get() {
+        return value;
     }
 
     public T orElse(T other) {
@@ -227,7 +332,7 @@ public class ChainX<T> {
         if (nonNull()) {
             return value;
         }
-        throw new NoSuchElementException("No value present");
+        throw new NoSuchElementException("Value is null");
     }
 
     public <X extends Throwable> T orElseThrow(X throwable) throws X {
