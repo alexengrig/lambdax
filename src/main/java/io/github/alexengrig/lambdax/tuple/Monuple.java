bad24c5ed130720fe60f65c0c87aac218941960c
Grig Alex <alexengrigdev@gmail.com>
2020-09-09 22:47:56 +0300

    Create Monuple and Valuable0

---
+++ src/main/java/io/github/alexengrig/lambdax/tuple/Monuple.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2019 - 2020 Alexengrig Dev.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.alexengrig.lambdax.tuple;
+
+import java.util.Collection;
+
+public class Monuple<T0> extends EmptyTuple implements Valuable0<T0> {
+    protected static final int SIZE = 1;
+
+    protected final T0 value0;
+
+    public Monuple(T0 value0) {
+        this.value0 = value0;
+    }
+
+    public Monuple(Collection<? extends T0> collection) {
+        this(collection.iterator().next());
+    }
+
+    @Override
+    public int size() {
+        return SIZE;
+    }
+
+    @Override
+    public <X> X getValue(int index) {
+        requireLegalIndex(index);
+        @SuppressWarnings("unchecked")
+        X target = (X) this.value0;
+        return target;
+    }
+
+    @Override
+    public T0 getValue0() {
+        return value0;
+    }
+
+    public <R0> Monuple<R0> setAt0(R0 value0) {
+        return new Monuple<>(value0);
+    }
+
+    public EmptyTuple removeAt0() {
+        return EmptyTuple.INSTANCE;
+    }
+
+}
