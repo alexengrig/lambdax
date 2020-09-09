60461615fb991c4954760376ccc74e61409311e1
Grig Alex <alexengrigdev@gmail.com>
2020-09-09 22:46:42 +0300

    Create EmptyTuple

---
+++ src/main/java/io/github/alexengrig/lambdax/tuple/EmptyTuple.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2019 - 2020 Alexengrig Dev.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.alexengrig.lambdax.tuple;
+
+public class EmptyTuple implements Tuple {
+    public static final EmptyTuple INSTANCE = new EmptyTuple();
+
+    protected static final int SIZE = 0;
+
+    protected EmptyTuple() {
+    }
+
+    @Override
+    public int size() {
+        return SIZE;
+    }
+
+    @Override
+    public <X> X getValue(int index) {
+        throw new IndexOutOfBoundsException("Empty tuple has no values");
+    }
+
+    protected void requireLegalIndex(int index) {
+        if (index < 0 || index >= size()) {
+            throw new IndexOutOfBoundsException(String.format("The index must be from 0 to %d (exclusive)", size()));
+        }
+    }
+}
